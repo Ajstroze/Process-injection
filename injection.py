b350716aeba26758de4532e95b7de4a791efec99
Ajstroze <67028694+Ajstroze@users.noreply.github.com>
2023-03-02 11:26:13 -0500

    initial code upload
    first upload of the script

---
+++ injection.py
@@ -0,0 +1,55 @@
+import pymem
+import subprocess
+import pwn
+
+def xorDecrypt(key, file):
+    output = []
+    for byte in file:
+        outbyte = pwn.xor(byte, key)
+        output.append(outbyte.decode('latin-1'))
+    return output
+
+def processInjection(process):
+    mem = pymem.Pymem(process)
+    mem.inject_python_interpreter()
+    return mem
+
+#code = """
+#print("Hello Injection!")
+#"""
+#mem.inject_python_shellcode(code)
+
+def getBytes(mem,address_list):
+    #address_list = [0x401577,0x40157b,0x40157f,0x401583,0x401587,0x40158b,0x40158f]
+    arr = []
+    for item in address_list:
+        arr.append(mem.read_bytes(int(item) + 3, 1).decode('latin-1'))
+    return arr
+
+def bruteForce(key,arr):
+    output = [arr]
+    for i in range(1,key+1):
+        output.append(xorDecrypt(i,arr))
+    return output
+
+def main(process,key,address_list):
+    mem = processInjection(process)
+    bytesArr = getBytes(mem,address_list)
+    bruteArr = bruteForce(key,bytesArr)
+    for i in range(0,key+1):
+        print(i,''.join(bruteArr[i]))
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description = 'Xor Brute force a particular location in memory of a program')
+    parser.add_argument('-p','--process', type=str,required=True,help='The name of the process to inject into')
+    parser.add_argument('-c','--count',type=int,required=True,help='What value to xor up to in decimal')
+    parser.add_argument('-a','--address',type=str, help='The memory location of the bytes to Xor, input should be a single string where each hex offet is seperated by a comma', required=True)
+    args = parser.parse_args()
+    address_list= [int(item,16) for item in args.address.split(',')]
+    main(args.process,args.count,address_list)
+
+# for i in range(256):
+#output = xorBruteforce(65, arr)
+#print(output)
